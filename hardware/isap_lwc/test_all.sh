424cd42d991387440674bce9a8915823bb95a7ea
Robert Primas <rprimas@student.tugraz.at>
2022-08-05 20:58:31 +0200

    test scripts

--- hardware/isap_lwc/test_all.sh
+++ hardware/isap_lwc/test_all.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 declare -a arr=(
-"test_v1.py"
-"test_v1_8bit.py"
-"test_v1_16bit.py"
-"test_v1_lowlatency.py"
-"test_v1_testmode1.py"
-"test_v1_StP.py"
-"test_v1_decfail.py"
-"test_v2.py"
+"v1"
+"v1_8bit"
+"v1_16bit"
+"v1_lowlatency"
+"v1_StP"
+"v1_decfail"
+"v2"
 )
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+make clean
 for i in "${arr[@]}"
 do
-	echo "$i"
-	res=$(python3 $i | grep "SIMULATION FINISHED")
+	echo "Running testbench for $i..."
+	res=$(make $i | grep "::")
 	if [[ $res == *"PASS"* ]]; then
-	  	printf "${GREEN}PASS!${NC}\n"
+		printf "${GREEN}PASS!${NC}\n"
 	else
-		printf "${RED}FAIL!${NC}\n"
+    		printf "${RED}FAIL!${NC}\n"
 	fi
+	make clean
 done
 
